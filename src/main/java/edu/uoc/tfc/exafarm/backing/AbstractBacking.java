792bb83813718b122bfcf61182e6b424fc7d0736
Franz Jimeno <franz.jimeno@gmail.com>
2011-06-03 12:11:14 +0200

    Empezando documentación (Navelgas)

--- src/main/java/edu/uoc/tfc/exafarm/backing/AbstractBacking.java
+++ src/main/java/edu/uoc/tfc/exafarm/backing/AbstractBacking.java
@@ -1,6 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Clase abstracta recoge valores del entorno para poder utilizarlos en 
+ * los diferentes Backing Beans
  */
 
 package edu.uoc.tfc.exafarm.backing;
@@ -30,10 +30,22 @@ public abstract class AbstractBacking {
     @ManagedProperty(value="#{viewScope}")
     private Map<String, Object> viewMap;
 
+    
+    /**
+     * Getter para el usuario que ha accedido. Los datos del usuario
+     * se encuentran guardados en la sesión.
+     * 
+     * @return Usuario que ha accedido 
+     */
     public Usuario getCurrentUser() {
         return (Usuario) getSessionMap().get("currentUser");
     }
 
+    /**
+     * Setter para el usuario que ha accedido.
+     * 
+     * @param currentUser Usuario que ha accedido.
+     */
     public void setCurrentUsuario(Usuario currentUser) {
         getSessionMap().remove("currentUser");
         if (null != currentUser) {
@@ -41,10 +53,21 @@ public abstract class AbstractBacking {
         }
     }
     
+    /**
+     * Getter para Examen. Permite consultar un examen seleccionado
+     * durante la vista.
+     * 
+     * @return Examen seleccionado
+     */
     public Examen getCurrentExamen() {
         return(Examen) getViewMap().get("currentExamen");
     }
     
+    /**
+     * Setter del Examen. Guarda el examen en el entorno vista.
+     * 
+     * @param currentExamen Examen a guardar.
+     */
     public void setCurrentExamen(Examen currentExamen) {
         getViewMap().remove("currentExamen");
         if (null != currentExamen) {
@@ -52,53 +75,112 @@ public abstract class AbstractBacking {
         }
     }
 
+    /**
+     * Método que comprueba si hay algún usuario registrado.
+     * 
+     * @return Boolean. True si hay un usuario registrado. 
+     */
     public boolean isUsuarioLoggedIn() {
         return getSessionMap().containsKey("currentUser");
     }
 
-    // The compiler can optimizes calls such as this into plain field accesses.
-    // Therefore, there is no performance penalty for always using the
-    // accessor.
+    /**
+     * Getter para FacesContext
+     * 
+     * @return FacesContext de la aplicación 
+     */
     public FacesContext getFacesContext() {
         return facesContext;
     }
 
+    /**
+     * Setter para FacesContext.
+     * 
+     * @param context
+     */
     public void setFacesContext(FacesContext context) {
         this.facesContext = context;
     }
 
+    /**
+     * Getter para SessionMap
+     * 
+     * @return Map<String, Object> actual. 
+     */
     public Map<String, Object> getSessionMap() {
         return sessionMap;
     }
 
+    /**
+     * Setter para SessionMap.
+     * 
+     * @param sessionMap Guarda un sessionMap
+     */
     public void setSessionMap(Map<String, Object> sessionMap) {
         this.sessionMap = sessionMap;
     }
 
+    /**
+     * Getter para RequestMap.
+     * 
+     * @return RequestMap
+     */
     public Map<String, Object> getRequestMap() {
         return requestMap;
     }
 
+    /**
+     * Setter para RequestMap.
+     * 
+     * @param requestMap 
+     */
     public void setRequestMap(Map<String, Object> requestMap) {
         this.requestMap = requestMap;
     }
 
+    /**
+     * Getter para ViewMap
+     * 
+     * @return viewMap 
+     */
     public Map<String, Object> getViewMap() {
         return viewMap;
     }
-
-    public void setViewMap(Map<String, Object> viewScope) {
-        this.viewMap = viewScope;
+    
+    /**
+     * Setter para ViewMap
+     * 
+     * @param viewScope 
+     */
+    public void setViewMap(Map<String, Object> viewMap) {
+        this.viewMap = viewMap;
     }
 
+    /**
+     * Getter para Flash.
+     * 
+     * @return flash 
+     */
     public Flash getFlash() {
         return getFacesContext().getExternalContext().getFlash();
     }
     
+    /**
+     * Añade un mensaje general que aparecerá en la visualización
+     * 
+     * @param message Texto que ha de aparecer
+     */
     public void addMessage(String message) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
     }
     
+    /**
+     * Añade un mensaje general con tipo de severidad.
+     * 
+     * @param severity severidad del mensaje
+     * @param message Texto que aparece
+     * 
+     */
     public void addMessage(FacesMessage.Severity severity, String message) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, message, null));
     }
